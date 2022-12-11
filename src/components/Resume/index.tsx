4c5b634e88a63d1417a44dd82eb021df6e3d66b4
Igor Santos <falaigors@gmail.com>
2022-12-10 22:45:51 -0300

    graficos de resumo

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -1,5 +1,15 @@
 export const Resume = () => {
   return (
-    <div className="max-w-lg w-full h-screen bg-zinc-50 p-8">ResultBar</div>
+    <div className="max-w-lg w-full h-screen bg-zinc-50 p-8 flex flex-col">
+      <span className="text-xl font-bold pb-4">Resume</span>
+      <div className="flex gap-8">
+        <div className="bg-zinc-100 drop-shadow-xl w-1/2 h-40 bg-[url('/chart-up.svg')] bg-cover bg-no-repeat bg-center rounded-3xl p-8 text-center">
+          <span className="text-xl font-bold">42 tasks</span>
+        </div>
+        <div className="bg-zinc-100 drop-shadow-xl w-1/2 h-40 bg-[url('/chart-down.svg')] bg-cover bg-no-repeat bg-center rounded-3xl p-8 text-center">
+          <span className="text-xl font-bold">9 issues</span>
+        </div>
+      </div>
+    </div>
   );
 };
