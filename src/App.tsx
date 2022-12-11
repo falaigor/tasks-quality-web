79fbf29470e4276c37ccb659c32e52624ec354cb
Igor Santos <falaigors@gmail.com>
2022-12-10 23:06:05 -0300

    adiconando biblioteca de icones

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Plus } from "phosphor-react";
 import { Resume } from "./components/Resume";
 import { Sidebar } from "./components/Sidebar";
 import { DefaultLayout } from "./layouts/Default";
@@ -9,7 +10,9 @@ export function App() {
       <div>
         <div className="flex justify-between">
           <h1 className="text-3xl font-bold mb-4">Welcome back Igor</h1>
-          <button>Add new task</button>
+          <button className="flex items-center gap-2 p-4 text-blue-500 hover:text-blue-900">
+            <Plus weight="bold" /> Add new task
+          </button>
         </div>
 
         <div className="bg-zinc-50 rounded-xl p-4">
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,14 +1,16 @@
+import { Clipboard, HouseSimple } from "phosphor-react";
+
 export function Sidebar() {
   return (
     <aside className="w-80 pl-8 pt-8">
       <img src="/logo-dark.svg" width="175px" alt="" />
       <nav className="mt-4">
         <ul>
-          <li className="p-4 my-2 text-zinc-50 bg-blue-500 rounded-xl">
-            Dashboard
+          <li className="p-4 my-2 text-zinc-50 bg-blue-500 rounded-xl flex gap-2 items-center">
+            <HouseSimple weight="bold" size={20} /> Dashboard
           </li>
-          <li className="p-4 my-2 hover:text-zinc-50 hover:bg-blue-500 rounded-xl">
-            Activity
+          <li className="p-4 my-2 hover:text-zinc-50 hover:bg-blue-500 rounded-xl flex gap-2 items-center">
+            <Clipboard weight="bold" size={20} /> Activity
           </li>
         </ul>
       </nav>
