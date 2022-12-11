698fc1f02c7e146b7634f0db1505d9ae3cbc0085
Igor Santos <falaigors@gmail.com>
2022-12-11 00:06:59 -0300

    instalando e iniciando configuracao do react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import { Plus } from "phosphor-react";
-import { Resume } from "./components/Resume";
-import { Sidebar } from "./components/Sidebar";
-import { DefaultLayout } from "./layouts/Default";
+import { AppRouter } from "./routes/AppRouter";
+
 import "./styles/main.css";
-import { TaskItem } from "./components/TaskItem/index";
 
 export function App() {
-  return (
-    <DefaultLayout>
-      <div>
-        <div className="flex justify-between">
-          <h1 className="text-3xl font-bold mb-4">Welcome back Igor</h1>
-          <button className="flex items-center gap-2 p-4 text-blue-500 hover:text-blue-900">
-            <Plus weight="bold" /> Add new task
-          </button>
-        </div>
-
-        <div className="bg-zinc-50 rounded-xl p-4">
-          <span className="text-xl font-bold">Week Tasks</span>
-
-          <TaskItem status="in-progress" />
-        </div>
-      </div>
-    </DefaultLayout>
-  );
+  return <AppRouter />;
 }
---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+export function AppRouter() {
+  return <RouterProvider router={router} />;
+}
