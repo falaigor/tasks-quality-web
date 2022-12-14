7bfa1103dcc2238df05ae6e4e92625f5c6e9a437
Igor Santos <falaigors@gmail.com>
2022-12-13 22:43:50 -0300

    feat: navigation to pages

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,23 +1,39 @@
 import { Checks, Clipboard, HouseSimple } from "phosphor-react";
+import { NavLink } from "react-router-dom";
 
 export function Sidebar() {
   const variant = {
     active: `p-4 my-2 gap-2 text-sm text-zinc-50 bg-primary rounded-xl flex flex-col justify-center items-center`,
     default: `p-4 my-2 gap-2 text-sm hover:text-zinc-50 hover:bg-primary transition-all rounded-xl flex flex-col justify-center items-center`,
   };
+
   return (
     <div className="w-32">
       <aside className="fixed w-32 p-4 bg-white rounded-2xl flex flex-col items-center">
         <img src="/favicon.svg" width="60px" alt="" />
         <nav className="mt-8">
           <ul>
-            <li className={variant.active}>
-              <HouseSimple weight="bold" size={20} />
-              Dashboard
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? variant.active : variant.default
+                }
+              >
+                <HouseSimple weight="bold" size={20} />
+                Dashboard
+              </NavLink>
             </li>
-            <li className={variant.default}>
-              <Checks weight="bold" size={20} />
-              Tasks
+            <li>
+              <NavLink
+                to="/tasks"
+                className={({ isActive }) =>
+                  isActive ? variant.active : variant.default
+                }
+              >
+                <Checks weight="bold" size={20} />
+                Tasks
+              </NavLink>
             </li>
           </ul>
         </nav>
