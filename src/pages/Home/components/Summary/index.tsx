0640bbad7e361d42ee63eef66652491154477a23
Igor Santos <falaigors@gmail.com>
2022-12-13 22:18:06 -0300

    change name summary item

--- src/pages/Home/components/Summary/index.tsx
+++ src/pages/Home/components/Summary/index.tsx
@@ -6,19 +6,17 @@ export function Summary() {
       <div className="flex gap-8">
         <div className="bg-primary text-zinc-100 w-1/3 h-44 rounded-2xl p-8 flex items-center justify-center  flex-col">
           <span className="text-6xl font-bold">42</span>
-          <p className="text-xl">tasks</p>
+          <p className="text-xl">Total tasks</p>
         </div>
 
         <div className="bg-primary text-zinc-100 w-1/3 h-44 rounded-2xl p-8 flex items-center justify-center flex-col">
           <span className="text-6xl font-bold">9</span>
-          <p className="text-xl">issues</p>
+          <p className="text-xl">Total issues</p>
         </div>
 
         <div className="bg-primary text-zinc-100 w-1/3 h-44 rounded-2xl p-8 flex items-center justify-center flex-col">
-          <span className="text-6xl font-bold">
-            90<span className="text-xl">%</span>
-          </span>
-          <p className="text-xl">Quality Tasks</p>
+          <span className="text-6xl font-bold">4.6</span>
+          <p className="text-xl">Issue average by tasks</p>
         </div>
       </div>
     </div>
