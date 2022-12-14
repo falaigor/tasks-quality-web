4659105a644bda4c148d6e6b8818448dd00bfd47
Igor Santos <falaigors@gmail.com>
2022-12-14 09:34:18 -0300

    feat: create modal add new issue

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,33 @@
+import { useCallback, useRef } from "react";
 import { Plus } from "phosphor-react";
 import { Summary } from "./components/Summary";
 import { TaskItem } from "@/components/TaskItem";
+import { Modal } from "@/components/Modal";
+
+interface IModal {
+  onOpen(): void;
+}
 
 export function HomePage() {
+  const modalRef = useRef<IModal>(null);
+  const handleClickOpenModal = useCallback(
+    () => modalRef?.current?.onOpen(),
+    []
+  );
+
   return (
     <div className="flex flex-col gap-10">
+      <Modal ref={modalRef} />
+
       <Summary />
 
       <div className="bg-zinc-50 rounded-xl p-9">
         <div className="flex justify-between items-center">
           <span className="text-2xl font-bold">Week Tasks</span>
-          <button className="flex items-center p-4 text-blue-500 hover:text-blue-900">
+          <button
+            onClick={() => handleClickOpenModal()}
+            className="flex items-center p-4 text-blue-500 hover:text-blue-900"
+          >
             <Plus weight="bold" /> Add new task
           </button>
         </div>
