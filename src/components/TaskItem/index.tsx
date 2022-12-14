6a68ca48dca83b93a25768e31df54b7e8bf5a20c
Igor Santos <falaigors@gmail.com>
2022-12-13 21:48:19 -0300

    finaly table tasks weeked

--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -10,13 +10,17 @@ export const TaskItem = ({ status }: TaskItemProps) => {
   };
 
   return (
-    <div className="flex rounded-2xl my-3 border-2 border-gray-200 justify-between items-center hover:bg-gray-100">
-      <div className={`ml-4 m-4 px-2 border-l-4 ${statusColor[status]}`}>
+    <div className="grid grid-cols-5 rounded-2xl my-3 border-2 border-gray-200 justify-between items-center hover:bg-gray-100">
+      <div
+        className={`ml-6 m-4 px-4 col-span-2 border-l-[5px] ${statusColor[status]}`}
+      >
         <span className="text-gray-300 text-xs font-bold">Team</span>
-        <p className="font-bold">Title</p>
+        <p className="font-medium">Um Titulo bem grande pra ver como fica </p>
       </div>
-      <div className="p-4 mr-4 rounded-full bg-yellow-300 bg-opacity-40 ring-2 ring-white flex items-center justify-center font-extrabold">
-        2 issues
+      <p className="text-center">12/02/2020</p>
+      <p className="text-center">12/02/2020</p>
+      <div className="p-4 mr-4 rounded-full bg-yellow-300 bg-opacity-40 ring-2 ring-white flex items-center justify-center font-semibold">
+        2
       </div>
     </div>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,19 +3,46 @@ import { Summary } from "./components/Summary";
 import { TaskItem } from "../../components/TaskItem";
 
 export function Home() {
+  const transactions = [
+    {
+      id: 1,
+      title: "O titulo maneiro de uma task",
+      date: new Date().getDay(),
+      issues: 0,
+      status: "in-progress",
+    },
+    {
+      id: 2,
+      title: "O titulo maneiro de uma task",
+      date: new Date().getDay(),
+      issuesQuantity: 0,
+      status: "waiting",
+    },
+  ];
+
   return (
     <div className=" flex flex-col gap-10">
       <Summary />
 
-      <div className="bg-zinc-50 rounded-xl p-4">
-        <div className="flex justify-between">
-          <span className="text-xl font-bold">Week Tasks</span>
-          <button className="flex items-center gap-2 p-4 text-blue-500 hover:text-blue-900">
+      <div className="bg-zinc-50 rounded-xl p-9">
+        <div className="flex justify-between items-center">
+          <span className="text-2xl font-bold">Week Tasks</span>
+          <button className="flex items-center p-4 text-blue-500 hover:text-blue-900">
             <Plus weight="bold" /> Add new task
           </button>
         </div>
 
-        <TaskItem status="in-progress" />
+        <div className="mt-8">
+          <div className="grid grid-cols-5 font-semibold justify-between items-center">
+            <p className="ml-7 px-4 col-span-2">Tasks</p>
+            <p className="text-center">Data de Inicio</p>
+            <p className="text-center">Data Final</p>
+            <p className="text-center mr-4 ">Issues</p>
+          </div>
+          <TaskItem status="in-progress" />
+          <TaskItem status="waiting" />
+          <TaskItem status="complete" />
+        </div>
       </div>
     </div>
   );
