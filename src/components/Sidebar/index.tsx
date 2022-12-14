81216d782b5bf1c3d387c023b784cc31fd7b3395
Igor Santos <falaigors@gmail.com>
2022-12-13 22:03:06 -0300

    update sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,17 +1,23 @@
-import { Clipboard, HouseSimple } from "phosphor-react";
+import { Checks, Clipboard, HouseSimple } from "phosphor-react";
 
 export function Sidebar() {
+  const variant = {
+    active: `p-4 my-2 gap-2 text-sm text-zinc-50 bg-primary rounded-xl flex flex-col justify-center items-center`,
+    default: `p-4 my-2 gap-2 text-sm hover:text-zinc-50 hover:bg-primary transition-all rounded-xl flex flex-col justify-center items-center`,
+  };
   return (
     <div className="w-32">
-      <aside className="fixed w-32 p-8 bg-white rounded-2xl">
+      <aside className="fixed w-32 p-4 bg-white rounded-2xl flex flex-col items-center">
         <img src="/favicon.svg" width="60px" alt="" />
-        <nav className="mt-4">
+        <nav className="mt-8">
           <ul>
-            <li className="p-4 my-2 text-zinc-50 bg-blue-500 rounded-xl flex justify-center items-center">
+            <li className={variant.active}>
               <HouseSimple weight="bold" size={20} />
+              Dashboard
             </li>
-            <li className="p-4 my-2 hover:text-zinc-800 hover:bg-blue-500 rounded-xl flex justify-center items-center">
-              <Clipboard weight="bold" size={20} />
+            <li className={variant.default}>
+              <Checks weight="bold" size={20} />
+              Tasks
             </li>
           </ul>
         </nav>
