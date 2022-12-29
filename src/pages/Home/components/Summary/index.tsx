ad5ce1adcff9763b90fcd9eb63a730411c8f9933
Igor Santos <falaigors@gmail.com>
2022-12-29 11:43:25 -0300

    styles: update spacing summary

--- src/pages/Home/components/Summary/index.tsx
+++ src/pages/Home/components/Summary/index.tsx
@@ -4,19 +4,19 @@ export function Summary() {
       <span className="text-3xl font-extrabold pb-4">Resume</span>
 
       <div className="flex gap-8">
-        <div className="bg-primary text-zinc-100 w-1/3 h-44 rounded-2xl p-8 flex items-center justify-center  flex-col">
+        <div className="bg-primary text-zinc-100 w-1/3 h-44 rounded-2xl p-8 flex items-center justify-center sm:justify-between  flex-col">
           <span className="text-6xl font-bold">42</span>
-          <p className="text-xl">Total tasks</p>
+          <p className="text-lg">Total tasks</p>
         </div>
 
-        <div className="bg-primary text-zinc-100 w-1/3 h-44 rounded-2xl p-8 flex items-center justify-center flex-col">
+        <div className="bg-primary text-zinc-100 w-1/3 h-44 rounded-2xl p-8 flex items-center justify-center sm:justify-between flex-col">
           <span className="text-6xl font-bold">9</span>
-          <p className="text-xl">Total issues</p>
+          <p className="text-lg">Total issues</p>
         </div>
 
-        <div className="bg-primary text-zinc-100 w-1/3 h-44 rounded-2xl p-8 flex items-center justify-center flex-col">
+        <div className="bg-primary text-zinc-100 w-1/3 h-44 rounded-2xl p-8 flex items-center justify-center sm:justify-between flex-col">
           <span className="text-6xl font-bold">4.6</span>
-          <p className="text-xl">Issue average by tasks</p>
+          <p className="text-lg">Issue average by tasks</p>
         </div>
       </div>
     </div>
